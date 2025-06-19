c7f2ccd19a4e1228976869c1e01e3e181cd497f3
John O’Sullivan <osullivanj.01@gmail.com>
2025-06-19 11:31:12 +0100

    Comment out fcknat to save on costs while not using

--- modules/nat-gateway/main.tf
+++ modules/nat-gateway/main.tf
@@ -1,13 +1,13 @@
-module "nat-gateway" {
-  source  = "RaJiska/fck-nat/aws"
-  version = "1.3.0"
+# module "nat-gateway" {
+#   source  = "RaJiska/fck-nat/aws"
+#   version = "1.3.0"
 
-  name      = "${var.infra_name}-${var.env}-fck-nat-gateway"
-  vpc_id    = var.vpc_id
-  subnet_id = var.public_subnets[0]
+#   name      = "${var.infra_name}-${var.env}-fck-nat-gateway"
+#   vpc_id    = var.vpc_id
+#   subnet_id = var.public_subnets[0]
 
-  update_route_tables = true
-  route_tables_ids = {
-    "private" = var.private_route_table_ids[0]
-  }
-}
+#   update_route_tables = true
+#   route_tables_ids = {
+#     "private" = var.private_route_table_ids[0]
+#   }
+# }
